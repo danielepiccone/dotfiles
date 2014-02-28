e58040fa0110b09de9a6d04014a772f5e00cbeec
Daniele Piccone <mild.taste@gmail.com>
2014-02-28 10:42:42 +0100

    multiple choices

--- link.sh
+++ link.sh
@@ -1,30 +1,44 @@
 #!/usr/bin/bash
 
+echo "Downloading modules...";
 git submodule init;
 git submodule update;
 
-echo "Copying .screenrc";
-cp ~/dotfiles/.screenrc ~/.screenrc
+read -p "Install .screenrc? (y/n) " -n 1
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    echo "Copying .screenrc";
+    cp ~/dotfiles/.screenrc ~/.screenrc
+fi
 
-echo "Copying .bashrc";
-cp ~/dotfiles/.bashrc ~/.bashrc
+read -p "Install .bashrc? (y/n) " -n 1
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    echo "Copying .bashrc";
+    cp ~/dotfiles/.bashrc ~/.bashrc
+fi
 
-echo "Copying VIM settings";
-cp ~/dotfiles/.vimrc ~/.vimrc
+read -p "Install .vimrc? (y/n) " -n 1
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
 
-echo "Installing pathogen"
-mkdir -p ~/.vim/autoload ~/.vim/bundle; 
-cp -r ~/dotfiles/pathogen/autoload/pathogen.vim ~/.vim/autoload/pathogen.vim
+    echo "Copying VIM settings";
+    cp ~/dotfiles/.vimrc ~/.vimrc
+fi
 
-echo "Copying VIM bundles";
-cp -r ~/dotfiles/bundle/ ~/.vim/
+read -p "Install VIm bundles and Pathogen? (y/n) " -n 1
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
 
-echo "Copying VIM colors";
-cp -r ~/dotfiles/colors/ ~/.vim/
+    echo "Installing pathogen"
+    mkdir -p ~/.vim/autoload ~/.vim/bundle; 
+    cp -r ~/dotfiles/pathogen/autoload/pathogen.vim ~/.vim/autoload/pathogen.vim
 
+    echo "Copying VIM bundles";
+    cp -r ~/dotfiles/bundle/ ~/.vim/
 
-#cd /etc
-#ln -s ~/dotfiles/.vimrc vimrc
-#cd ~
+    echo "Copying VIM colors";
+    cp -r ~/dotfiles/colors/ ~/.vim/
+fi
 
-echo "End.";
+echo "Stay well.";
