50b99ca463ef2786ae46737173facedc04131c30
Daniele Piccone <dpi@one.com>
2018-09-21 12:33:10 +0200

    adding bin to quickly start a MR on gitlab

--- link.sh
+++ link.sh
@@ -1,24 +1,7 @@
-#!/usr/bin/bash
+#!/bin/bash
+echo "Linking bins..."
 
-read -p "Install .screenrc? (y/n) " -n 1
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-    echo "Linking .screenrc";
-    ln -s ~/dotfiles/.screenrc ~/.screenrc
-fi
-
-read -p "Install .bashrc? (y/n) " -n 1
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-    echo "Linking .bashrc";
-    ln -s ~/dotfiles/.bashrc ~/.bashrc
-fi
-
-read -p "Install z? (y/n) " -n 1
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-    echo "Linking /bin/local/z";
-    ln -s ~/dotfiles/bin/z /usr/local/bin/z
-fi
+ln -s ~/dotfiles/bin/z /usr/local/bin/z
+ln -s ~/dotfiles/bin/gitlab-mr /usr/local/bin/gitlab-mr
 
 echo "Stay well.";
