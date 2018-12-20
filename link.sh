a309bc8a72a041faa85c90adc178f1328aef6a3f
Daniele Piccone <dpi@one.com>
2018-12-20 08:52:34 +0100

    script to get latests commits i have done in projects

--- link.sh
+++ link.sh
@@ -3,5 +3,6 @@ echo "Linking bins..."
 
 ln -s ~/dotfiles/bin/z /usr/local/bin/z
 ln -s ~/dotfiles/bin/gitlab-mr /usr/local/bin/gitlab-mr
+ln -s ~/dotfiles/bin/whatdidido /usr/local/bin/whatdidido
 
 echo "Stay well.";
