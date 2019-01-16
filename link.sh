46f28c5f8f934a200e9e005fc6e2ab0654d698c3
Daniele Piccone <dpi@one.com>
2019-01-16 10:46:22 +0100

    bin/git-stats

--- link.sh
+++ link.sh
@@ -3,6 +3,7 @@ echo "Linking bins..."
 
 ln -s ~/dotfiles/bin/z /usr/local/bin/z
 ln -s ~/dotfiles/bin/gitlab-mr /usr/local/bin/gitlab-mr
+ln -s ~/dotfiles/bin/git-stats /usr/local/bin/git-stats
 ln -s ~/dotfiles/bin/whatdidido /usr/local/bin/whatdidido
 
 echo "Stay well.";
