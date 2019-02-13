7e87199e9eef6fedac38957c00136955194bd9ce
Daniele Piccone <dpi@one.com>
2019-02-13 13:11:00 +0100

    add feh to deps

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "Installing dependencies..."
 
-dependencies="shellcheck build-essential"
+dependencies="shellcheck build-essential feh"
 
 for dependency in $dependencies; do
   sudo apt install $dependency
