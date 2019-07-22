83a05da61304da167e514872c6afd5fbd2be4dc6
Daniele Piccone <mild.taste@gmail.com>
2019-07-22 11:31:13 +0200

    add docker-purge link

--- link.sh
+++ link.sh
@@ -5,5 +5,6 @@ ln -s ~/dotfiles/bin/z /usr/local/bin/z
 ln -s ~/dotfiles/bin/gitlab-mr /usr/local/bin/gitlab-mr
 ln -s ~/dotfiles/bin/git-stats /usr/local/bin/git-stats
 ln -s ~/dotfiles/bin/whatdidido /usr/local/bin/whatdidido
+ln -s ~/dotfiles/bin/docker-purge /usr/local/bin/docker-purge
 
 echo "Stay well.";
