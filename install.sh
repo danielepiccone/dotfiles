24bc883d26f619f89ed4fcfa5b30624939d0670f
Daniele Piccone <you@example.com>
2020-07-07 16:24:51 +0200

    grouped installation

--- install.sh
+++ install.sh
@@ -1,16 +1,44 @@
-#!/bin/sh
+#!/usr/bin/env bash
 echo "Installing dependencies..."
 
 if test "$(uname)" = "Darwin"; then
-  dependencies="shellcheck coreutils findutils gnu-tar gnu-sed gawk gnutls gnu-indent gnu-getopt grep vim ansible"
+  dependencies="shellcheck coreutils findutils gnu-tar gnu-sed gawk gnutls gnu-indent gnu-getopt grep vim stow ansible"
   echo "Installing $dependencies with homebrew."
   brew install $dependencies
-  exit 0
 fi
 
 if test "$(uname)" = "Linux"; then
   dependencies="shellcheck build-essential stow vim ansible feh curl ack"
   echo "Installing $dependencies with apt."
-  sudo apt-get install $dependencies
+  sudo apt-get install $dependencies -y
+fi
+
+echo "Linking dotfiles..."
+
+stow bash
+stow vim
+stow tmux
+stow fonts
+
+echo "Building bins..."
+
+(cd fzf && make)
+
+echo "Linking bins..."
+
+sudo ln -s ~/dotfiles/bin/fzf /usr/local/bin/fzf
+sudo ln -s ~/dotfiles/bin/z /usr/local/bin/z
+sudo ln -s ~/dotfiles/bin/git-stats /usr/local/bin/git-stats
+sudo ln -s ~/dotfiles/bin/docker-purge /usr/local/bin/docker-purge
+
+echo "Installing applications..."
+
+if test "$(uname)" = "Darwin"; then
+  echo "Installing application is not supported on Darwin."
+  exit 0
+fi
+
+if test "$(uname)" = "Linux"; then
+  (cd ansible && ./provision.sh)
   exit 0
 fi
--- install_symlinks.sh
+++
@@ -1,8 +0,0 @@
-stow bash
-stow vim
-stow tmux
-stow fonts
-
-# Only provision
-cp -r vscode/.config/Code/ ~/.config/
-
